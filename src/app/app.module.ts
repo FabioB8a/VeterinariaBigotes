a54eb532b598e9e669976c2c66dff23622a1f391
andres345gm <andres345gm@gmail.com>
2023-09-24 11:15:55 -0500

    app module corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetTableComponent } from './pet/pet-table/pet-table.component';
-import { PetDetailsComponent } from './pet/pet-details/pet-details.component';
 import { PetDetailComponent } from './pet/pet-detail/pet-detail.component';
 import { PetFormComponent } from './pet/pet-form/pet-form.component';
 
@@ -12,7 +11,6 @@ import { PetFormComponent } from './pet/pet-form/pet-form.component';
   declarations: [
     AppComponent,
     PetTableComponent,
-    PetDetailsComponent,
     PetDetailComponent,
     PetFormComponent
   ],
