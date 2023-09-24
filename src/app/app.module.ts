26a5d5bbd00ab39929ab99736b75b5e2ce908af0
andres345gm <andres345gm@gmail.com>
2023-09-24 11:00:07 -0500

    Added the initial component and model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetTableComponent } from './pet/pet-table/pet-table.component';
+import { PetDetailsComponent } from './pet/pet-details/pet-details.component';
+import { PetDetailComponent } from './pet/pet-detail/pet-detail.component';
+import { PetFormComponent } from './pet/pet-form/pet-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetTableComponent,
+    PetDetailsComponent,
+    PetDetailComponent,
+    PetFormComponent
   ],
   imports: [
     BrowserModule,
