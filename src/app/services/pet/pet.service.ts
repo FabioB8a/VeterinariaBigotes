0bfde1a2c2b418825efcb1279885b89c88cff493
FabioB8a <fabiobuitra@gmail.com>
2023-09-24 14:21:05 -0500

    ADD: Navigation between all pets and details

--- src/app/services/pet/pet.service.ts
+++ src/app/services/pet/pet.service.ts
@@ -14,11 +14,22 @@ export class PetService {
       'Firulais',
       'Chihuahua',
       new Date('2019-01-01'),
-      5,
+      500,
       'Parvovirus',
       'https://images.pexels.com/photos/160722/cat-tiger-getiegert-feel-at-home-160722.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       'Juan Perez'
+    ),
+    new Pet(
+      2,
+      'Bella',
+      'Siberian',
+      new Date('2022-07-14'),
+      500,
+      'Ninguna',
+      'https://images.pexels.com/photos/1056252/pexels-photo-1056252.jpeg?auto=compress&cs=tinysrgb&w=400',
+      'Andrés Perez'
     )
+  
   ];
 
   findAll() {
