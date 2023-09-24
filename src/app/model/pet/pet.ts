b73e373c74dbac71514de5b2957bef6c84518b78
FabioB8a <fabiobuitra@gmail.com>
2023-09-24 11:41:08 -0500

    Add pet model

--- src/app/model/pet/pet.ts
+++ src/app/model/pet/pet.ts
@@ -1,2 +1,42 @@
 export class Pet {
+
+    id: number;
+    name: string;
+    breed: string;
+    birthdate: Date;
+    weight: number;
+    disease: string;
+    imgUrl: string;
+    status: string;
+    owner: string;
+
+    constructor(
+        id: number,
+        name: string,
+        breed: string,
+        birthdate: Date,
+        weight: number,
+        disease: string,
+        imgUrl: string,
+        owner: string
+    ) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+        this.birthdate = birthdate;
+        this.weight = weight;
+        this.disease = disease;
+        this.imgUrl = imgUrl;
+        this.status = 'En tratamiento';
+        this.owner = owner;
+    }
+
+    calculateAge(): number {
+        const currentDate = new Date();
+        const birthDate = this.birthdate;
+        const ageInMilliseconds = currentDate.getTime() - birthDate.getTime();
+        const ageInSeconds = ageInMilliseconds / 1000;
+        const ageInYears = ageInSeconds / (60 * 60 * 24 * 365.25);
+        return Math.floor(ageInYears);
+    }
 }
