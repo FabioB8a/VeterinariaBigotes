f4d50da5df92cb51df2cafa6f8dcb04d0265f9ad
FabioB8a <fabiobuitra@gmail.com>
2023-09-24 14:42:08 -0500

    ADD: Save pet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PetTableComponent} from "./pet/pet-table/pet-table.component";
 import {PetDetailComponent} from "./pet/pet-detail/pet-detail.component";
+import { PetFormComponent } from './pet/pet-form/pet-form.component';
 
 const routes: Routes = [
   { path: 'pet/all', component: PetTableComponent },
-  { path : 'pet/detail/:id', component : PetDetailComponent}
+  { path : 'pet/detail/:id', component : PetDetailComponent},
+  { path: 'pet/save', component: PetFormComponent}
 ];
 
 @NgModule({
