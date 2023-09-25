5e2dc9891f35666a17c52c199262c0c5ff23810a
FabioB8a <fabiobuitra@gmail.com>
2023-09-25 17:08:17 -0500

    ADD: Owner model ts

--- src/app/model/owner/owner.ts
+++ src/app/model/owner/owner.ts
@@ -1,2 +1,30 @@
+import { Pet } from "../pet/pet";
+
 export class Owner {
-}
+    id: number;
+    idCard: number;
+    firstName: string;
+    firstLastName: string;
+    secondLastName: string;
+    phone: string;
+    email: string;
+    pets!: Pet[];
+  
+    constructor(
+      id: number,
+      idCard: number,
+      firstName: string,
+      firstLastName: string,
+      secondLastName: string,
+      phone: string,
+      email: string
+    ) {
+      this.id = id;
+      this.idCard = idCard;
+      this.firstName = firstName;
+      this.firstLastName = firstLastName;
+      this.secondLastName = secondLastName;
+      this.phone = phone;
+      this.email = email;
+    }
+  }
