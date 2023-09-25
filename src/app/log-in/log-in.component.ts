d9c458b1630b2be0859388a97ba47d89cf1b1a5d
parraluisa <parran_ll@javeriana.edu.co>
2023-09-25 10:51:46 -0500

    log-in component updated

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -6,60 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent {
+  private formActive: HTMLElement | null = null;
+  private loginVet: HTMLElement | null = null;
+  private loginOwner: HTMLElement | null = null;
 
-  loginVet: HTMLElement | null = null;
-  loginOwner: HTMLElement | null = null;
-  formActive: HTMLElement | null = null;
+  constructor(private router:Router) {}  
 
-  constructor() {
-    // Initialize DOM elements
-    this.loginVet = document.querySelector(".loginVet");
-    this.loginOwner = document.querySelector(".loginOwner");
-    this.formActive = document.querySelector(".container-forms");
+  ngOnInit(): void {
+    this.formActive = document.querySelector('.container-forms');
+    this.loginVet = document.querySelector('.loginVet');
+    this.loginOwner = document.querySelector('.loginOwner');
   }
 
   tab1(): void {
-    if (this.formActive) {
-      this.formActive.style.marginLeft = "0";
-    }
+    if (this.formActive) this.formActive.style.marginLeft = '0';
 
     if (this.loginVet) {
-      this.loginVet.style.opacity = "0.7";
-      const vetImage = this.loginVet.querySelector("img");
-      if (vetImage) vetImage.style.opacity = "0.7";
+      this.loginVet.style.opacity = '0.7';
+      const vetImage = this.loginVet.querySelector('img');
+      if (vetImage) vetImage.style.opacity = '0.7';
     }
 
     if (this.loginOwner) {
-      this.loginOwner.style.opacity = "";
-      const ownerImage = this.loginOwner.querySelector("img");
-      if (ownerImage) ownerImage.style.opacity = "";
+      this.loginOwner.style.opacity = '';
+      const ownerImage = this.loginOwner.querySelector('img');
+      if (ownerImage) ownerImage.style.opacity = '';
     }
   }
 
   tab2(): void {
-    if (this.formActive) {
-      this.formActive.style.marginLeft = "-97%";
-    }
+    if (this.formActive) this.formActive.style.marginLeft = '-97%';
 
     if (this.loginVet) {
-      this.loginVet.style.opacity = "1";
-      const vetImage = this.loginVet.querySelector("img");
-      if (vetImage) vetImage.style.opacity = "1";
+      this.loginVet.style.opacity = '1';
+      const vetImage = this.loginVet.querySelector('img');
+      if (vetImage) vetImage.style.opacity = '1';
     }
 
     if (this.loginOwner) {
-      this.loginOwner.style.opacity = "0.7";
-      const ownerImage = this.loginOwner.querySelector("img");
-      if (ownerImage) ownerImage.style.opacity = "0.7";
+      this.loginOwner.style.opacity = '0.7';
+      const ownerImage = this.loginOwner.querySelector('img');
+      if (ownerImage) ownerImage.style.opacity = '0.7';
     }
   }
 
   onSubmit(type: string): void {
-    // Handle form submission based on the type (owner or vet)
     if (type === 'owner') {
-      // Handle owner form submission
+      const idCardOwner = document.getElementById('idCardOwner') as HTMLInputElement;
+      const idOwner = idCardOwner.value;
+
+      if (idOwner === '') {
+        alert('Por favor ingrese su cédula');
+      }
     } else if (type === 'vet') {
-      // Handle vet form submission
+      const idCardVet = document.getElementById('idCardVet') as HTMLInputElement;
+      const passwordVet = document.getElementById('passwordVet') as HTMLInputElement;
+
+      const idVet = idCardVet.value;
+      const password = passwordVet.value;
+      this.router.navigate(['/pet/all']);
+      /*
+      if (idVet === '') {
+        alert('Por favor ingrese su cédula');
+      }
+      if (password === '') {
+        alert('Por favor ingrese su contraseña');
+      }*/
     }
   }
 }
