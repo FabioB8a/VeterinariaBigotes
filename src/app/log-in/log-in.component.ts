70c24759dffbdb537302d05bd934c92f39af04a1
parraluisa <parran_ll@javeriana.edu.co>
2023-10-09 17:28:27 -0500

    Vista de log in arreglada

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -5,94 +5,100 @@ import { UserService } from '../services/user/UserService';
 
 
 @Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+    selector: 'app-log-in',
+    templateUrl: './log-in.component.html',
+    styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent {
-  private formActive: HTMLElement | null = null;
-  private loginVet: HTMLElement | null = null;
-  private loginOwner: HTMLElement | null = null;
-  userType: string='';  // Variable para almacenar el tipo de usuario
+    private formActive: HTMLElement | null = null;
+    private loginVet: HTMLElement | null = null;
+    private loginOwner: HTMLElement | null = null;
+    userType: string='';  // Variable para almacenar el tipo de usuario
 
-  constructor(
-    private router: Router,
-    private ownerService: OwnerService ,
-    private userService: UserService
-  ) {}
+    constructor(
+        private router: Router,
+        private ownerService: OwnerService ,
+        private userService: UserService
+    ) {}
 
-  ngOnInit(): void {
-    this.formActive = document.querySelector('.container-forms');
-    this.loginVet = document.querySelector('.loginVet');
-    this.loginOwner = document.querySelector('.loginOwner');
-  }
+    ngOnInit(): void {
+        this.formActive = document.querySelector('.container-forms');
+        this.loginVet = document.querySelector('.loginVet');
+        this.loginOwner = document.querySelector('.loginOwner');
 
-  tab1(): void {
-    if (this.formActive) this.formActive.style.marginLeft = '0';
-
-    if (this.loginVet) {
-      this.loginVet.style.opacity = '0.7';
-      const vetImage = this.loginVet.querySelector('img');
-      if (vetImage) vetImage.style.opacity = '0.7';
+        localStorage.removeItem('userType');
     }
 
-    if (this.loginOwner) {
-      this.loginOwner.style.opacity = '';
-      const ownerImage = this.loginOwner.querySelector('img');
-      if (ownerImage) ownerImage.style.opacity = '';
+    tab1(): void {
+        if (this.formActive) this.formActive.style.marginLeft = '0';
+
+        if (this.loginVet) {
+            this.loginVet.style.opacity = '0.7';
+            const vetImage = this.loginVet.querySelector('img');
+            if (vetImage) vetImage.style.opacity = '0.7';
+        }
+
+        if (this.loginOwner) {
+            this.loginOwner.style.opacity = '';
+            const ownerImage = this.loginOwner.querySelector('img');
+            if (ownerImage) ownerImage.style.opacity = '';
+        }
     }
-  }
 
-  tab2(): void {
-    if (this.formActive) this.formActive.style.marginLeft = '-97%';
+    tab2(): void {
+        if (this.formActive) this.formActive.style.marginLeft = '-97%';
 
-    if (this.loginVet) {
-      this.loginVet.style.opacity = '1';
-      const vetImage = this.loginVet.querySelector('img');
-      if (vetImage) vetImage.style.opacity = '1';
+        if (this.loginVet) {
+            this.loginVet.style.opacity = '1';
+            const vetImage = this.loginVet.querySelector('img');
+            if (vetImage) vetImage.style.opacity = '1';
+        }
+
+        if (this.loginOwner) {
+            this.loginOwner.style.opacity = '0.7';
+            const ownerImage = this.loginOwner.querySelector('img');
+            if (ownerImage) ownerImage.style.opacity = '0.7';
+        }
     }
 
-    if (this.loginOwner) {
-      this.loginOwner.style.opacity = '0.7';
-      const ownerImage = this.loginOwner.querySelector('img');
-      if (ownerImage) ownerImage.style.opacity = '0.7';
-    }
-  }
+    onSubmit(type: string): void {
+        if (type === 'owner') {
+            this.userType = 'owner';
+            const idCardOwner = document.getElementById('idCardOwner') as HTMLInputElement;
+            const idOwner = +idCardOwner.value;
+            console.log("El usuario es", idOwner);
+            if (!idOwner) {
+                alert('Por favor ingrese su cédula');
+            } else {
+                const owner = this.ownerService.login(idOwner);
+                if (owner) {
+                    // Suponiendo que tienes acceso al UserService
+                    this.userService.setUserType('user');  // O 'vet' dependiendo del tipo
+                    this.router.navigate(['/pet/all']);
+                }
+                else {
+                    alert("El usuario no existe");
+                }
+            }
+        } else if (type === 'vet') {
+            console.log("HOLAAAA", type);
 
-  onSubmit(type: string): void {
-    if (type === 'owner') {
-      this.userType = 'owner';
-      const idCardOwner = document.getElementById('idCardOwner') as HTMLInputElement;
-      const idOwner = +idCardOwner.value;
-      console.log("El usuario es", idOwner);
-      if (!idOwner) {
-        alert('Por favor ingrese su cédula');
-      } else {
-        const owner = this.ownerService.login(idOwner);
-        if (owner) {
-            // Suponiendo que tienes acceso al UserService
-            this.userService.setUserType('user');  // O 'vet' dependiendo del tipo
+            this.userType = 'vet';
+            const idCardVet = document.getElementById('idCardVet') as HTMLInputElement;
+            const passwordVet = document.getElementById('passwordVet') as HTMLInputElement;
+
+            const idVet = idCardVet.value;
+            const password = passwordVet.value;
+            //ACA
+            this.userService.setUserType('vet');
             this.router.navigate(['/pet/all']);
+            /*
+            if (idVet === '') {
+              alert('Por favor ingrese su cédula');
+            }
+            if (password === '') {
+              alert('Por favor ingrese su contraseña');
+            }*/
         }
-        else {
-          alert("El usuario no existe");
-        }
-      }
-    } else if (type === 'vet') {
-        this.userType = 'vet';
-      const idCardVet = document.getElementById('idCardVet') as HTMLInputElement;
-      const passwordVet = document.getElementById('passwordVet') as HTMLInputElement;
-
-      const idVet = idCardVet.value;
-      const password = passwordVet.value;
-      this.router.navigate(['/pet/all']);
-      /*
-      if (idVet === '') {
-        alert('Por favor ingrese su cédula');
-      }
-      if (password === '') {
-        alert('Por favor ingrese su contraseña');
-      }*/
     }
-  }
 }
--- src/app/pet/pet-table/pet-table.component.ts
+++ src/app/pet/pet-table/pet-table.component.ts
@@ -4,51 +4,73 @@ import {PetService} from "../../services/pet/pet.service";
 import {UserService} from "../../services/user/UserService";
 
 @Component({
-  selector: 'app-pet-table',
-  templateUrl: './pet-table.component.html',
-  styleUrls: ['./pet-table.component.css']
+    selector: 'app-pet-table',
+    templateUrl: './pet-table.component.html',
+    styleUrls: ['./pet-table.component.css']
 })
 export class PetTableComponent {
 
-  @Input() userType: string ='';
+    @Input() userType: string ='';
 
-  selectedPet!: Pet;
+    selectedPet!: Pet;
 
-  petList!: Pet[];
-
-
-  constructor(
-    private petService: PetService
-    ,private userService: UserService
-  ) {
-      this.userType = this.userService.getUserType();
-  }
+    petList!: Pet[];
 
 
-  filterText: string = '';
-  isNameFilterActive: boolean = false;
+    constructor(
+        private petService: PetService
+        ,private userService: UserService
+    ) {
 
-  ngOnInit(): void {
-    this.petService.findAll().subscribe(
-      data => this.petList = data.map(x => Object.assign(new Pet(x.id,x.name,x.breed,x.birthdate,x.weight,x.disease,x.imgUrl), x))
-    )
-
-  }
-
-  // Correcciones posteriores
-  filterPetsByName() {
-    if (this.isNameFilterActive) {
-      return this.petList.filter(pet =>
-        pet.name.toLowerCase().includes(this.filterText.toLowerCase())
-      );
-    } else {
-      return this.petList; // If the checkbox is not active, return the full list
     }
-  }
 
-  deleteById(pet:Pet):void{
-    const index = this.petList.indexOf(pet);
-    this.petList.splice(index,1);
-    this.petService.deleteById(pet.id);
-  }
+
+    filterText: string = '';
+    isNameFilterActive: boolean = false;
+
+    ngOnChanges(): void {
+
+    }
+
+    ngOnInit(): void {
+
+        //
+        this.userType = this.userService.getUserType();
+        //localStorage == coockies
+        let type =  localStorage.getItem('userType');
+
+        console.log("antes",this.userType);
+
+
+        if(type == 'vet'){
+            this.userType = 'vet';
+        }else if (type == 'user'){
+            this.userType = 'user';
+        }else {
+            localStorage.setItem('userType',this.userType);
+        }
+        console.log("despues",this.userType);
+
+        this.petService.findAll().subscribe(
+            data => this.petList = data.map(x => Object.assign(new Pet(x.id,x.name,x.breed,x.birthdate,x.weight,x.disease,x.imgUrl), x))
+        )
+
+    }
+
+    // Correcciones posteriores
+    filterPetsByName() {
+        if (this.isNameFilterActive) {
+            return this.petList.filter(pet =>
+                pet.name.toLowerCase().includes(this.filterText.toLowerCase())
+            );
+        } else {
+            return this.petList; // If the checkbox is not active, return the full list
+        }
+    }
+
+    deleteById(pet:Pet):void{
+        const index = this.petList.indexOf(pet);
+        this.petList.splice(index,1);
+        this.petService.deleteById(pet.id);
+    }
 }
