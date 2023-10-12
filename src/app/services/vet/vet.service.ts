f42f75e5fad03f1293d51548a83a2278d8c401af
FabioB8a <fabiobuitra@gmail.com>
2023-10-12 15:24:03 -0500

    ADD: Table component and service

--- src/app/services/vet/vet.service.ts
+++ src/app/services/vet/vet.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Veterinarian } from 'src/app/model/veterinarian/veterinarian';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VetService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  vetList: Veterinarian[] = [];
+
+  findAll(): Observable<Veterinarian[]>{
+    return this.http.get<Veterinarian[]>('http://localhost:8090/owner');
+  }
+
+  findById(id:number ): Observable<Veterinarian>{
+    return this.http.get<Veterinarian>("http://localhost:8090/vet/" + id);
+  }
+
+  addOwner(newVet: Veterinarian) {
+    this.http.post("http://localhost:8090/vet", newVet).subscribe();
+  }
+
+  updateOwner(changedVet: Veterinarian) {
+    this.http.put("http://localhost:8090/vet", changedVet).subscribe();
+  }
+
+  deleteById(id: number) {
+    this.http.delete("http://localhost:8090/vet/" + id).subscribe();
+  }
+
 }
--- src/app/vet/vet-table/vet-table.component.ts
+++ src/app/vet/vet-table/vet-table.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Veterinarian } from 'src/app/model/veterinarian/veterinarian';
+import { VetService } from 'src/app/services/vet/vet.service';
 
 @Component({
   selector: 'app-vet-table',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./vet-table.component.css']
 })
 export class VetTableComponent {
+
+  vetList!: Veterinarian[];
+  filterText: string = '';
+  isNameFilterActive: boolean = false;
+
+  constructor(
+    private vetService: VetService
+  ) {}
+
+  ngOnInit(): void {
+    this.vetService.findAll().subscribe(
+      data => {
+        this.vetList = data.map(x => Object.assign(new Veterinarian(x.id, x.idCard, x.firstName, x.firstLastName, x.secondLastName, x.password, x.speciality, x.imgUrl), x));
+        console.log(this.vetList); // Mueve el console.log aquí
+      }
+    );
+  }
+
+  filterVetsByName(){
+    if(this.isNameFilterActive){
+      return this.vetList.filter(vet => vet.firstName.toLowerCase().includes(this.filterText.toLowerCase()));
+
+    } else {
+      return this.vetList;
+    }
+  }
+
+  deleteById(vet:Veterinarian):void{
+    const index = this.vetList.indexOf(vet);
+    this.vetList.splice(index,1);
+    this.vetService.deleteById(vet.id);
+  }
 
 }
