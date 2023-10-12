141728e65951a44292aaedd6ddee4e93ef14a8e1
FabioB8a <fabiobuitra@gmail.com>
2023-10-12 16:13:21 -0500

    FINISH: Vet detail component

--- src/app/vet/vet-detail/vet-detail.component.ts
+++ src/app/vet/vet-detail/vet-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Veterinarian } from 'src/app/model/veterinarian/veterinarian';
+import { VetService } from 'src/app/services/vet/vet.service';
 
 @Component({
   selector: 'app-vet-detail',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class VetDetailComponent {
 
+  @Input()
+  vet!: Veterinarian;
+
+  constructor(
+    private vetService: VetService,
+    private route: ActivatedRoute,
+    private router: Router)
+  {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.vetService.findById(id).subscribe((data) => {
+      this.vet = new Veterinarian(data.id, data.idCard, data.firstName, data.firstLastName, data.secondLastName, data.password, data.speciality, data.imgUrl);
+    });
+  }
 }
