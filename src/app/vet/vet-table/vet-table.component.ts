43acfb9709073a3a2fa3a9357305408f9bc4c8e9
FabioB8a <fabiobuitra@gmail.com>
2023-10-12 14:40:14 -0500

    ADD: Components vet

---
+++ src/app/vet/vet-table/vet-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vet-table',
+  templateUrl: './vet-table.component.html',
+  styleUrls: ['./vet-table.component.css']
+})
+export class VetTableComponent {
+
+}
