a508257f657e20e253b14e2e5579117cc93405b0
FabioB8a <fabiobuitra@gmail.com>
2023-10-15 17:49:28 -0500

    CHANGE: Routing between detail, form and table

--- src/app/pet/pet-form/pet-form.component.ts
+++ src/app/pet/pet-form/pet-form.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { Pet } from 'src/app/model/pet/pet';
-import {PetService} from "../../services/pet/pet.service";
-import {OwnerService} from "../../services/owner/owner.service";
-import {ActivatedRoute, Router} from '@angular/router';
-import { Owner } from 'src/app/model/owner/owner';
+import { PetService } from "../../services/pet/pet.service";
+import { OwnerService } from "../../services/owner/owner.service";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pet-form',
@@ -15,29 +14,32 @@ export class PetFormComponent {
   constructor(private petService: PetService, private ownerService: OwnerService, private route: ActivatedRoute, private router: Router) { }
 
   sendPet!: Pet;
-
-  formPet!: Pet
+  formPet!: Pet;
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       if ('petId' in params) {
-          const petId = Number(params['petId']);
+        const petId = Number(params['petId']);
 
-          this.petService.findById(petId).subscribe(data => {
-              this.formPet = new Pet(data.id, data.name, data.breed, data.birthdate, data.weight, data.disease, data.imgUrl, data.owner);
+        this.petService.findById(petId).subscribe(data => {
+          this.formPet = new Pet(data.id, data.name, data.breed, data.birthdate, data.weight, data.disease, data.imgUrl, data.owner);
 
-              this.ownerService.findOwnerByPets_Id(petId).subscribe(owner => {
-                  this.formPet.owner = owner;
-              });
+          this.ownerService.findOwnerByPets_Id(petId).subscribe(owner => {
+            this.formPet.owner = owner;
           });
+        });
       }
-  });
+    });
   }
-
 
   savePet() {
     // Change logic from owner
     this.sendPet = Object.assign({}, this.formPet);
+    console.log(this.sendPet);
+    console.log(this.formPet);
+
     this.petService.addPet(this.formPet);
+
+    this.router.navigate(['/pet/all'], {queryParams: {type: "vet"}});
   }
 }
