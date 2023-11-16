12bea05f7e809c273dab0daa24dccdd53558f5e3
parraluisa <parran_ll@javeriana.edu.co>
2023-11-16 18:16:00 -0500

    Last version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PetTableComponent} from "./pet/pet-table/pet-table.component";
-import {PetDetailComponent} from "./pet/pet-detail/pet-detail.component";
-import { PetFormComponent } from './pet/pet-form/pet-form.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { LandingComponent } from './landing/landing.component';
 import {OwnerTableComponent} from "./owner/owner-table/owner-table.component";
-import {OwnerFormComponent} from "./owner/owner-form/owner-form.component";
-import {OwnerDetailComponent} from "./owner/owner-detail/owner-detail.component";
 import { VetTableComponent } from './vet/vet-table/vet-table.component';
-import { VetFormComponent } from './vet/vet-form/vet-form.component';
-import { VetDetailComponent } from './vet/vet-detail/vet-detail.component';
 import { DashBoardComponent} from "./admin/dash-board/dash-board.component";
 import { AdminLogInComponent} from "./admin/admin-log-in/admin-log-in.component";
 import {PetDetailsComponent} from "./pet/pet-details/pet-details.component";
@@ -23,12 +17,6 @@ const routes: Routes = [
   { path: 'pet/all', component: PetTableComponent },
   {path: 'owner/all', component: OwnerTableComponent},
   {path: 'vet/all', component: VetTableComponent},
-  //{ path : 'pet/detail/:id', component : PetDetailComponent},
-  //{ path : 'owner/detail/:id', component : OwnerDetailComponent},
-  //{ path : 'vet/detail/:id', component : VetDetailComponent},
-  { path: 'pet/save', component: PetFormComponent},
-  { path: 'owner/save', component: OwnerFormComponent},
-  { path: 'vet/save', component: VetFormComponent},
   { path: 'login/show', component: LogInComponent},
   { path: 'admin/dashboard', component: DashBoardComponent},
   { path: 'admin/login', component: AdminLogInComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetTableComponent } from './pet/pet-table/pet-table.component';
-import { PetDetailComponent } from './pet/pet-detail/pet-detail.component';
-import { PetFormComponent } from './pet/pet-form/pet-form.component';
 import {NgOptimizedImage} from "@angular/common";
 import { LogInComponent } from './log-in/log-in.component';
 import { LandingComponent } from './landing/landing.component';
 import { FormsModule } from '@angular/forms';
-import { OwnerDetailComponent } from './owner/owner-detail/owner-detail.component';
-import { OwnerFormComponent } from './owner/owner-form/owner-form.component';
 import { OwnerTableComponent } from './owner/owner-table/owner-table.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { VetFormComponent } from './vet/vet-form/vet-form.component';
-import { VetDetailComponent } from './vet/vet-detail/vet-detail.component';
 import { VetTableComponent } from './vet/vet-table/vet-table.component';
 import { DashBoardComponent } from './admin/dash-board/dash-board.component';
 import { AdminLogInComponent } from './admin/admin-log-in/admin-log-in.component';
@@ -30,15 +24,9 @@ import { OwnerModalComponent } from './owner/owner-modal/owner-modal.component';
   declarations: [
     AppComponent,
     PetTableComponent,
-    PetDetailComponent,
-    PetFormComponent,
     LogInComponent,
     LandingComponent,
-    OwnerDetailComponent,
-    OwnerFormComponent,
     OwnerTableComponent,
-    VetFormComponent,
-    VetDetailComponent,
     VetTableComponent,
     DashBoardComponent,
     AdminLogInComponent,
